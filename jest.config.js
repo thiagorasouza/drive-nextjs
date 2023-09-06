d27f81e023e221dfa5da3f44168d81b23b23f4e4
Thiago Souza <thiagorasouza@gmail.com>
2023-09-06 17:11:10 -0500

    fix: jest config for filenamify package

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  // Needs to transform JS files form filenamify and filename-reserved-regex packages
+  preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  transformIgnorePatterns: [
+    // Removes packages folders from ignore pattern
+    "<rootDir>/node_modules/(?!(?:filenamify|filename-reserved-regex))",
+  ],
 };
