0561574073dc5947a81a7f5df3364263523f4ee0
Thiago Souza <thiagorasouza@gmail.com>
2023-09-07 11:19:34 -0500

    feat: add FilePath Value Object

---
+++ src/lib/domain/filePath.ts
@@ -0,0 +1,25 @@
+import { InvalidParam } from "../core/failures/invalidParam";
+import { Success } from "../core/success";
+
+export class FilePath {
+  private readonly _value: string;
+
+  private constructor(filePath: string) {
+    this._value = filePath;
+  }
+
+  public get value(): string {
+    return this._value;
+  }
+
+  public static create(
+    filePath: string,
+    validator: Validator,
+  ): InvalidParam | Success<FilePath> {
+    if (!validator.isValid(filePath)) {
+      return new InvalidParam("filePath");
+    }
+
+    return new Success<FilePath>(new FilePath(filePath));
+  }
+}
